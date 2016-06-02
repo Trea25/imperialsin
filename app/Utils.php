1d4683ce97ef6d7c329057ce76bdcdb5b17bac85
Trea25 <trea25@hotmail.com>
2016-06-02 19:49:34 +0200

    comentaris utils

--- app/Utils.php
+++ app/Utils.php
@@ -10,6 +10,12 @@ class Utils
         'Content-Type' => 'application/json; charset=utf-8'
     );
 
+    /**
+     * Funció que rep una data (dd/mm/yyyy) i una hora (dd:mm) i la formateja en formate date apte per la Base de Dades
+     * @param $date data en format dd/mm/yyyy
+     * @param $time hora en format hh:mm
+     * @return string Un string amb la data en format yyyy-mm-dd hh:mm:ss
+     */
     public static function date_formater($date, $time)
     {
         $date = explode("/", $date);
@@ -17,6 +23,11 @@ class Utils
         return $format;
     }
 
+    /**
+     * Funció per separar la data de yyyy-mm-dd hh:mm:ss a una data dd/mm/yyyy i una hora hh:mm
+     * @param $date Data en format yyyy-mm-dd hh:mm:ss
+     * @return string Data en format dd/mm/yyyy hh:mm
+     */
     public static function separate_date($date)
     {
         $date = explode(" ", $date);
@@ -25,6 +36,13 @@ class Utils
         $date=$date[2]."/".$date[1]."/".$date[0];
         return $date." ".$time;
     }
+
+    /**
+     * Metode que donat un fitxer o una imatge provinent d'el request ens l'escala, rota i retalla segons els parametres donats, els quals tambe venen del request
+     * @param Request $request HTTP Request amb l'imatge i/o els parametres de configuracio
+     * @param $file fitxer amb l'imatge provinent de base de dades la qual tractarem amb l'informacio provinent del request
+     * @return null
+     */
     public static function editImage(Request $request, $file){
 
         $img = null;
